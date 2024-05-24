02687a2113abfd9d263e532f422e9bc3bf0274b6
Jayavani9 <jayavaniakkiraju09@gmail.com>
2024-05-23 20:01:41 -0700

    Updated

--- peekingIterator.java
+++ peekingIterator.java
@@ -1,8 +1,49 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+// Tc: O(1) Sc: O(1) for all the methods
+/* 
+class PeekingIterator implements Iterator<Integer> {
+    Iterator<Integer> itr;
+    Integer val = null;
+      public PeekingIterator(Iterator<Integer> iterator) {
+          // initialize any member here.
+        itr = iterator;
+          if(itr.hasNext()) val = itr.next();
+        
+      }
+      
+      // Returns the next element in the iteration without advancing the iterator.
+      public Integer peek() {
+      
+      return val;
+          
+      }
+      
+      // hasNext() and next() should behave the same as in the Iterator interface.
+      // Override them if needed.
+      @Override
+      public Integer next() {
+        
+        Integer ans = val;
+        if(itr.hasNext()) val = itr.next();
+        else val = null;
+        return ans;
+          
+      }
+      
+      @Override
+      public boolean hasNext() {
+          return val != null || itr.hasNext();
+      }
+  }
+
+  */
+
 import java.util.*;
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
-//Tc: O(1) Sc: O(1) for all the methods
+// Tc: O(1) Sc: O(1) for all the methods
 class PeekingIterator implements Iterator<Integer> {
     Iterator<Integer> itr;
     Integer peekVal = null;
