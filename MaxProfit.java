6c9d308338ea5fdff08de81a397a1d40b78b355e
Karan Shah <karanshah1561998@gmail.com>
2024-09-30 23:22:52 -0400

    Competitive-Coding-10-20240929

---
+++ MaxProfit.java
@@ -0,0 +1,18 @@
+// Problem 122. Best Time to Buy and Sell Stock II
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
+    }
+}
